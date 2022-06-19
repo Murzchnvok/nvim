a71ce177693fe8516fc5aa3bb4b0afc7a7761433
Murzchnvok <murzchnvok@gmail.com>
2022-06-19 08:43:24 -0300

    update

--- nvim/lua/plugins/config/dap-ui.lua
+++ nvim/lua/plugins/config/dap-ui.lua
@@ -1,29 +1,19 @@
 require("dapui").setup({
-	sidebar = {
-		elements = {
-			{ id = "scopes", size = 0.25 },
-			{ id = "breakpoints", size = 0.25 },
-			{ id = "stacks", size = 0.25 },
-			{ id = "watches", size = 0.25 },
+	layouts = {
+		{
+			elements = {
+				{ id = "scopes", size = 0.25 },
+				{ id = "breakpoints", size = 0.25 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "watches", size = 0.25 },
+			},
+			size = 50,
+			position = "left",
 		},
-		size = 50,
-		position = "left",
-	},
-	tray = {
-		elements = {},
-		size = 10,
-		position = "bottom",
-	},
-	floating = {
-		max_height = nil,
-		max_width = nil,
-		border = "single",
-		mappings = {
-			close = { "q", "<Esc>" },
+		{
+			elements = {},
+			size = 10,
+			position = "bottom",
 		},
-	},
-	windows = { indent = 1 },
-	render = {
-		max_type_length = nil,
 	},
 })
